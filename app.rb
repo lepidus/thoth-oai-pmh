03ad98167687f07f1eb72904ac7ed054bc59604c
Thiago Brasil <thiago@lepidus.com.br>
2025-07-04 18:06:38 -0400

    feat: add OAI XSL stylesheet
    issue: documentacao-e-tarefas/desenvolvimento_e_infra#1035
    
    Signed-off-by: Thiago Brasil <thiago@lepidus.com.br>

--- app.rb
+++ app.rb
@@ -12,4 +12,19 @@ end
 get '/oai' do
   content_type 'application/xml; charset=utf-8'
   response = settings.oai_provider.process_request(params)
+  response.gsub(
+      '<?xml version="1.0" encoding="UTF-8"?>',
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<?xml-stylesheet type=\"text/xsl\" href=\"/oai2.xsl\"?>"
+    )
+end
+
+get '/oai2.xsl' do
+  content_type 'text/xsl; charset=utf-8'
+
+  begin
+    File.read(File.join(settings.root, 'assets', 'oai2.xsl'))
+  rescue Errno::ENOENT
+    status 404
+    "XSL stylesheet not found"
+  end
 end
