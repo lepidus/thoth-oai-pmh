e3b99b2bd3c559a8d1e05a6fcfa17e4e7e592524
Thiago Brasil <thiago@lepidus.com.br>
2025-07-04 19:10:13 -0400

    feat: create OAI model for Thoth
    issue: documentacao-e-tarefas/desenvolvimento_e_infra#1035
    
    Signed-off-by: Thiago Brasil <thiago@lepidus.com.br>

---
+++ oai/model.rb
@@ -0,0 +1,13 @@
+require 'oai'
+
+module ThothOAI
+  class Model < OAI::Provider::Model
+    def earliest
+      '2023-02-13T16:17:44Z'
+    end
+
+    def latest
+      '2023-04-20T14:45:30Z'
+    end
+  end
+end
--- oai/provider.rb
+++ oai/provider.rb
@@ -1,4 +1,5 @@
 require 'oai'
+require_relative 'model'
 
 module ThothOAI
   class Provider < OAI::Provider::Base
@@ -6,6 +7,7 @@ module ThothOAI
     repository_url 'http://localhost:4567/oai'
     record_prefix 'thoth'
     admin_email 'admin@example.com'
+    source_model Model.new
 
     register_format(OAI::Provider::Metadata::DublinCore.instance)
   end
