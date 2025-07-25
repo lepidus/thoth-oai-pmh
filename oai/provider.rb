5874f9d6297e12865dfd4012a5cd4f6a952d5f64
Thiago Brasil <thiago@lepidus.com.br>
2025-07-25 14:09:06 -0400

    fix: set Thoth identify
    issue: documentacao-e-tarefas/desenvolvimento_e_infra#1047
    
    Signed-off-by: Thiago Brasil <thiago@lepidus.com.br>

--- oai/provider.rb
+++ oai/provider.rb
@@ -8,12 +8,11 @@ module Thoth
     # OAI-PMH Provider class
     class Provider < OAI::Provider::Base
       repository_name 'Thoth OAI-PMH Repository'
-      repository_url 'http://localhost:4567/oai'
+      repository_url 'https://oai.thoth.pub/'
       record_prefix 'thoth'
-      admin_email 'admin@example.com'
+      admin_email 'support@thoth.pub'
+      sample_id '5a08ff03-7d53-42a9-bfb5-7fc81c099c52'
       source_model Model.new
-
-      register_format(OAI::Provider::Metadata::DublinCore.instance)
     end
   end
 end
