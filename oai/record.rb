f3eee10b0c9cc62024b8658689b25d17eaffdee7
Thiago Brasil <thiago@lepidus.com.br>
2025-07-29 18:10:42 -0400

    feat: restructure Record for mappers
    issue: documentacao-e-tarefas/desenvolvimento_e_infra#1047
    
    Signed-off-by: Thiago Brasil <thiago@lepidus.com.br>

--- oai/record.rb
+++ oai/record.rb
@@ -4,33 +4,18 @@ module Thoth
   module Oai
     # Thoth OAI-PMH record
     class Record
-      attr_accessor :id, :doi, :publications, :full_title, :creators,
-                    :contributors, :license, :publication_date, :publisher,
-                    :languages, :work_type, :keywords, :abstract,
-                    :relations, :updated_at
+      attr_accessor :json_record
 
-      def initialize(attributes = {})
-        attributes.each do |name, value|
-          instance_variable_set("@#{name}", value)
-        end
+      def initialize(json_record)
+        @json_record = json_record
       end
 
-      def map_oai_dc
-        {
-          identifiers: [:doi, :publications.map { |pub| pub['isbn'] }, :id],
-          title: :full_title,
-          creators: :creators,
-          contributors: :contributors,
-          license: :license,
-          date: :publication_date,
-          publisher: :publisher,
-          language: :languages,
-          type: :type,
-          subject: :keywords,
-          description: :abstract,
-          relations: :relations,
-          updatedAt: :updated_at
-        }
+      def id
+        @json_record[:id]
+      end
+
+      def updated_at
+        Time.parse(@json_record[:updated_at])
       end
     end
   end
--- tests/oai/test_record.rb
+++ tests/oai/test_record.rb
@@ -11,32 +11,11 @@ require 'webmock/test_unit'
 class RecordTest < Test::Unit::TestCase
   include Rack::Test::Methods
 
-  def test_create_new_record
-    attributes = {
-      id: '4f4a4dcb-2d88-43b6-8400-bd24926903b8',
-      doi: 'https://doi.org/10.1234/test-doi',
-      publications: [{ 'type' => 'PDF', 'isbn' => '978-3-123456-123-1' }],
-      full_title: 'Test Title',
-      creators: ['John Doe'],
-      contributors: ['Jane Smith'],
-      license: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
-      publication_date: '2021-10-26',
-      publisher: 'Thoth Publishers',
-      languages: ['eng'],
-      work_type: 'book',
-      keywords: ['test'],
-      abstract: 'This is a abstract for the work.',
-      relations: [
-        'https://doi.org/10.1234/related-work',
-        '978-3-123456-123-2'
-      ],
-      updated_at: '2021-10-26T12:00:00Z'
-    }
+  def test_get_id_and_updated_at
+    json_record = { id: '58c1832f-d7bc-4f80-8b89-3c91efd1d397', updated_at: '2023-10-01T12:00:00Z' }
+    record = Thoth::Oai::Record.new(json_record)
 
-    record = Thoth::Oai::Record.new(attributes)
-    assert_instance_of Thoth::Oai::Record, record
-    attributes.each do |name, value|
-      assert_equal value, record.send(name)
-    end
+    assert_equal '58c1832f-d7bc-4f80-8b89-3c91efd1d397', record.id
+    assert_equal Time.parse('2023-10-01T12:00:00Z'), record.updated_at
   end
 end
