e045fc0c6c49624693f89153a7812153acc2c34e
Thiago Brasil <thiago@lepidus.com.br>
2025-08-07 21:27:04 -0400

    feat: restructure oai_dc mapper to support updated metadata
    issue: documentacao-e-tarefas/desenvolvimento_e_infra#1047
    
    Signed-off-by: Thiago Brasil <thiago@lepidus.com.br>

--- oai/mapper/oai_dc.rb
+++ oai/mapper/oai_dc.rb
@@ -9,108 +9,127 @@ module Thoth
     module Mapper
       # Maps Thoth records to OAI DC format
       class OaiDc
+        TYPE_MAPPING = {
+          'HARDBACK' => 'hardback',
+          'PAPERBACK' => 'paperback',
+          'PDF' => 'application/pdf',
+          'EPUB' => 'application/epub+zip',
+          'XML' => 'text/xml',
+          'HTML' => 'text/html',
+          'DOCX' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+          'MP3' => 'audio/mpeg',
+          'WAV' => 'audio/wav',
+          'MOBI' => 'application/x-mobipocket-ebook',
+          'AZW3' => 'application/vnd.amazon.ebook',
+          'FICTION_BOOK' => 'application/x-fictionbook+xml'
+        }.freeze
+
         def initialize(input)
           @input = input
           metadata_format = OAI::Provider::Metadata::DublinCore.instance
           @header_specification = metadata_format.header_specification
-          @fields = metadata_format.fields
         end
 
         def map
           xml = Builder::XmlMarkup.new
           xml.tag!('oai_dc:dc', @header_specification) do
-            @fields.each do |field|
-              next unless respond_to?("#{field}_value")
-
-              value = send("#{field}_value")
-
-              next if value.nil? || value.empty?
-
-              value.respond_to?(:each) ? value.each { |v| xml.tag!("dc:#{field}", v) } : xml.tag!("dc:#{field}", value)
-            end
+            build_tags(xml)
           end
           xml.target!
         end
 
-        def title_value
-          @input['fullTitle']
+        def build_tags(xml)
+          build_title_tag(xml)
+          build_creator_tag(xml)
+          build_subject_tag(xml)
+          build_description_tag(xml)
+          build_publisher_tag(xml)
+          build_contributor_tag(xml)
+          build_date_tag(xml)
+          build_type_tag(xml)
+          build_format_tag(xml)
+          build_identifier_tag(xml)
+          build_language_tag(xml)
+          build_relation_tag(xml)
+          build_rights_tag(xml)
         end
 
-        def creator_value
-          @input['creator'].map { |creator| creator['fullName'] }.compact
+        def build_title_tag(xml)
+          xml.tag! 'dc:title', @input['fullTitle']
         end
 
-        def subject_value
-          @input['keywords'].map { |keyword| keyword['subjectCode'] }.compact
+        def build_creator_tag(xml)
+          @input['creator']&.each do |creator|
+            xml.tag! 'dc:creator', creator['fullName']
+          end
         end
 
-        def description_value
-          @input['longAbstract']
+        def build_subject_tag(xml)
+          @input['subjects']&.each do |subject|
+            xml.tag! 'dc:subject', subject['subjectCode'] if subject['subjectType'] == 'KEYWORD'
+          end
         end
 
-        def publisher_value
-          @input['imprint']['publisher']['publisherName']
+        def build_description_tag(xml)
+          xml.tag! 'dc:description', @input['longAbstract'] if @input['longAbstract']
         end
 
-        def contributor_value
-          @input['contributor'].map { |contributor| contributor['fullName'] }.compact
+        def build_publisher_tag(xml)
+          publisher = @input.dig('imprint', 'publisher', 'publisherName')
+          xml.tag! 'dc:publisher', publisher if publisher
         end
 
-        def date_value
-          @input['publicationDate']
+        def build_contributor_tag(xml)
+          @input['contributor']&.each do |contributor|
+            xml.tag! 'dc:contributor', contributor['fullName']
+          end
         end
 
-        def type_value
-          @input['workType'].downcase
+        def build_date_tag(xml)
+          xml.tag! 'dc:date', @input['publicationDate'] if @input['publicationDate']
         end
 
-        def format_value
-          types = {
-            'HARDBACK' => 'hardback',
-            'PAPERBACK' => 'paperback',
-            'PDF' => 'application/pdf',
-            'EPUB' => 'application/epub+zip',
-            'XML' => 'text/xml',
-            'HTML' => 'text/html',
-            'DOCX' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-            'MP3' => 'audio/mpeg',
-            'WAV' => 'audio/wav',
-            'MOBI' => 'application/x-mobipocket-ebook',
-            'AZW3' => 'application/vnd.amazon.ebook',
-            'FICTION_BOOK' => 'application/x-fictionbook+xml'
-          }
-
-          @input['publications'].map do |publication|
-            next unless types.key?(publication['publicationType'])
-
-            types[publication['publicationType']]
-          end.compact
+        def build_type_tag(xml)
+          type = case @input['workType']
+                 when 'JOURNAL_ISSUE' then 'issue'
+                 when 'BOOK_CHAPTER' then 'chapter'
+                 else 'book'
+                 end
+          xml.tag! 'dc:type', type
         end
 
-        def identifier_value
-          ["https://thoth.pub/books/#{@input['workId']}", @input['doi']] +
-            @input['publications'].map do |pub|
-              pub['isbn'] ? "urn:isbn:#{pub['isbn']}" : nil
-            end.compact
+        def build_format_tag(xml)
+          @input['publications']&.each do |format|
+            xml.tag! 'dc:format', TYPE_MAPPING[format['publicationType']]
+          end
         end
 
-        def language_value
-          @input['language'].map { |lang| lang['languageCode'].downcase }.compact
+        def build_identifier_tag(xml)
+          xml.tag! 'dc:identifier', "https://thoth.pub/books/#{@input['workId']}"
+          xml.tag! 'dc:identifier', @input['doi'] if @input['doi']
+          @input['publications']&.each do |publication|
+            xml.tag! 'dc:identifier', "urn:isbn:#{publication['isbn']}" if publication['isbn']
+          end
         end
 
-        def relation_value
-          @input['relations'].map do |relation|
-            [
-              relation['relatedWork']['doi'],
-              relation['relatedWork']['publications'].map do |pub|
-                pub['isbn'] ? "urn:isbn:#{pub['isbn']}" : nil
-              end
-            ].flatten.compact
-          end.flatten.compact
+        def build_language_tag(xml)
+          @input['language']&.each do |lang|
+            xml.tag! 'dc:language', lang['languageCode'].downcase
+          end
         end
 
-        def rights_value
-          @input['license']
+        def build_relation_tag(xml)
+          @input['relations']&.each do |relation|
+            related_work = relation['relatedWork']
+            xml.tag! 'dc:relation', related_work['doi'] if related_work['doi']
+            related_work['publications']&.each do |pub|
+              xml.tag! 'dc:relation', "urn:isbn:#{pub['isbn']}" if pub['isbn']
+            end
+          end
+        end
+
+        def build_rights_tag(xml)
+          xml.tag! 'dc:rights', @input['license'] if @input['license']
         end
       end
     end
